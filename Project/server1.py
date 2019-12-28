a8a447b9de3e19d4343cea487a3f546f6efc8324
James Q <jamesleroyq@gmail.com>
2019-12-28 19:27:35 +0000

    finished app server

--- Project/server1.py
+++ Project/server1.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
-patient=[
+patients=[
     {"id":1, "Name":"James Bond", "Diagnosis": "Myocardial Infarc", "DOB": "1960/01/28", "Doctor": "Dr. Murphy", "Gender": "Male"},
     {"id":2, "Name":"John Doe", "Diagnosis": "Gastric Ulcer", "DOB": "1970/09/02", "Doctor": "Dr. O'Shea", "Gender": "Male"},
     {"id":3, "Name":"Joyce James", "Diagnosis": "Dementia", "DOB": "1940/11/15", "Doctor": "Dr. Green", "Gender": "Female"},
@@ -16,25 +16,66 @@ nextId=6
 # def index():
 #     return 'Hello, World!'
 
-@app.route('/patient')
+@app.route('/patients')
 def getAll():
+    return jsonify(patients)
+
+@app.route('/patients/<int:id>')
+def findById(id):
+    foundPatient =  list(filter(lambda b: b['id']==id, patients))
+    if len(foundPatient) == 0:
+        return jsonify ({}), 204
+
+    return jsonify(foundPatient[0])
+
+# curl -i -H "Content-Type:application/json" -X POST -d "{\"Name\":\"Jay Leno\", \"Diagnosis\":\"STI\",\"DOB\":\"1950/07/15\",\"Doctor\":\"Dr. Zimmer\",\"Gender\":\"Male\"}" http://127.0.0.1:5000/patients
+@app.route('/patients', methods = ['POST'])
+def create():
+    global nextId
+    if not request.json:
+        abort(400)
+    # other checking
+    patient = {
+        "id" : nextId,
+        "Name": request.json['Name'],
+        "Diagnosis": request.json['Diagnosis'],
+        "DOB": request.json['DOB'],
+        "Doctor": request.json['Doctor'],
+        "Gender": request.json['Gender'],
+    }
+    nextId += 1 
+    patients.append(patient)
     return jsonify(patient)
 
-@app.route('/patient/<int:id>')
-def findById(id):
-    return "in find By ID for id " +str(id)
-
-@app.route('/patient', methods = ['POST'])
-def create():
-    return "in create"
-
-@app.route('/patient/<int:id>', methods=['PUT'])
+# curl -i -H "Content-Type:application/json" -X PUT -d "{\"Name\":\"Jay Leno\", \"Diagnosis\":\"STI\",\"DOB\":\"1950/07/15\",\"Doctor\":\"Dr. Zimmer\",\"Gender\":\"Male\"}" http://127.0.0.1:5000/patients/1
+@app.route('/patients/<int:id>', methods=['PUT'])
 def update(id):
-    return "in update for id " +str(id)
+    foundPatients =  list(filter(lambda b: b['id']==id, patients))
+    if len(foundPatients) ==0:
+        abort(404)
+    foundPatient = foundPatients[0]
+    if not request.json:
+        abort(400)
+    reqJson = request.json
+    if 'Name' in reqJson:
+        foundPatient['Name'] = reqJson['Name']
+    if 'Diagnosis' in reqJson:
+        foundPatient['Diagnosis'] = reqJson['Diagnosis']
+    if 'DOB' in reqJson:
+        foundPatient['DOB'] = reqJson['DOB']
+    if 'Doctor' in reqJson:
+        foundPatient['Doctor'] = reqJson['Doctor']
+    if 'Gender' in reqJson:
+        foundPatient['Gender'] = reqJson['Gender']
+    return jsonify(foundPatient)
 
-@app.route('/patient/<int:id>',methods=['DELETE'])
+@app.route('/patients/<int:id>',methods=['DELETE'])
 def delete(id):
-    return "in delete for id "+str(id)
+    foundPatients =  list(filter(lambda b: b['id']==id, patients))
+    if len(foundPatients) ==0:
+        abort(404)
+    patients.remove(foundPatients[0])
+    return jsonify({"done":True})
 
 if __name__ == '__main__':
     app.run(debug= True)
